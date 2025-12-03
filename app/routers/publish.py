53a8cbd0aa63fabd9ef6b86e4224a42cf7b631ab
kensan0123 <ken10232a@gmail.com>
2025-12-03 11:31:28 +0000

    publish 個人開発を始めました！Zenn CLIをFastAPIでラップしてLLMと接続してみます

--- app/routers/publish.py
+++ app/routers/publish.py
@@ -2,6 +2,7 @@ from dataclasses import dataclass
 from fastapi import APIRouter
 from pydantic import BaseModel
 from app.services.publish_service import PublishService, PublishResult
+from typing import Literal
 
 router = APIRouter()
 publish_service = PublishService()
@@ -9,13 +10,18 @@ publish_service = PublishService()
 class PublishRequest(BaseModel):
     slug: str
 
+class PublishResponse(BaseModel):
+    status: Literal["published!", "failed"]
+    slug: str
+
 @router.post("/")
-def publish_article_api(req: PublishRequest):
+def publish_article_api(req: PublishRequest) -> PublishResponse:
     result: PublishResult = publish_service.publish_article(req.slug)
     if result.result:
-        return {
-            "status": "published!",
-            "slug": req.slug,
-        }
+        publish_response: PublishResponse = PublishResponse(status="published!", slug=req.slug)
+
+        return publish_response
     else:
-        return False
+        publish_response: PublishResponse = PublishResponse(status="failed", slug=req.slug)
+
+        return publish_response
